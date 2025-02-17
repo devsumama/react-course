40e579c7f7d38a1fd47a51c03161c0e4da58a665
devsumama <sumamahassan120@email.com>
2025-02-17 15:39:08 +0500

    functional base rending in react & basics

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+function App(){
+    let myname='sumama'
+    return <div>
+        <h1 
+        // inline css
+        // style={{backgroundColor:'red'
+        //     ,color:'white',padding:'10px'
+        //     ,textAlign:'center'}}
+
+            >
+            hello app component</h1> 
+        <p>i am sumama</p>;
+        <img src="codding.webp" alt="codding" />
+        {myname}
+
+    </div>
+}
+export default App;
